0c73a80878957d9052709f7b90cab48792d64c18
dolefeast <elrois456yt@gmail.com>
2023-05-04 14:02:33 +0200

    changed up some figs

--- code/PklinOlinPsm.py
+++ code/PklinOlinPsm.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib
 #plt.style.use('fivethirtyeight')
 plt.rc('lines', linewidth=2.5)
 #matplotlib.use('pgf') #Saves the output as pgf
@@ -32,35 +33,49 @@ for i, data in enumerate([pklin, psmooth, Olin]):
     fig, ax = plt.subplots()
     ax.spines['top'].set_visible(False), ax.spines['right'].set_visible(False)
     ax.set_xscale('log'), ax.set_yscale('log')
-    fig.set_tight_layout(True)
     idx = np.where(np.logical_and(data[0]<=kmax, data[0]>=kmin))
     x, y = np.array(data[0]), np.array(data[1])
     x = x[idx]
     y = y[idx]
 
-    ax.plot(x, y, color='teal')
+    ax.plot(x, y, color='teal', linewidth=3)
 #    ax.axhline(y=min(y), color='black', linewidth=1.3, alpha=0.7)
 #    ax.axvline(x=kmin, color='black', linewidth=1.3, alpha=0.7)
-    ax.set_xlabel(r'$k [h $Mpc$^{-1}] $', fontsize=fontsize) 
+    ax.set_xlabel(r'$\log_{10}k [h $Mpc$^{-1}] $', fontsize=fontsize) 
     ax.set_xticks((0.05, 1e-1, 0.45))
     ax.set_xticklabels((0.05, 1e-1, 0.45), fontsize=fontsize)
-    yticks = np.linspace(min(y), max(y), 4)
+    logx = np.log10(x)
+    logy = np.log10(y)
+    xticks = np.logspace(min(logx), max(logx), 4, base=10)
+    yticks = np.logspace(min(logy), max(logy), 4, base=10)
 
     if i==0:
         yticks = [int(x) for x in np.round(yticks, -2)]
-        ax.set_yticks(yticks)
-        ax.set_yticklabels(yticks, fontsize=fontsize/1.3)
-        ax.set_ylabel(r'$\log_{10} P(k) [$Mpc$^3 h^{-3}]$', fontsize=fontsize)
-        plt.savefig('../figs/Pklin.png')
+        ylabel = r'$\log_{10} P(k) [$Mpc$^3 h^{-3}]$' 
+        name  = 'Pklin'
+        print('First plot done!')
     elif i==1:
         yticks = [int(x) for x in np.round(yticks, -2)]
-        ax.set_yticks(yticks)
-        ax.set_yticklabels(yticks, fontsize=fontsize/1.3)
-        ax.set_ylabel(r'$\log_{10} P_{smooth}(k) [$Mpc$^3 h^{-3}]$', fontsize=fontsize)
-        plt.savefig('../figs/Psm.png')
-    else:
-        ax.set_ylabel(r'$\log_{10} O_{lin}(k)$', fontsize=fontsize)
-        plt.savefig('../figs/Olin.png')
+        ylabel = r'$\log_{10} P_{smooth}(k) [$Mpc$^3 h^{-3}]$' 
+        name = 'Psm'
+        print('Second plot done!')
+    elif i==2:
+        yticks = [x for x in np.round(yticks, 3)]
+        ylabel = r'$\log_{10} O_{lin}(k)$'
+        name = 'Olin'
+        print('Third plot done!')
+
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(np.round(xticks, 2), fontsize=fontsize)
+
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(yticks, fontsize=fontsize)
+
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+
+    fig.set_tight_layout(True)
+    plt.savefig(f'../figs/{name}.pdf')
+    plt.savefig(f'../figs/{name}.png')
 
 #    ax.plot(psmooth[0],psmooth[1], color='teal')
 #    ax.plot(Olin[0],Olin[1], color='teal')
--- code/rustico_plot.py
+++ code/rustico_plot.py
@@ -25,7 +25,7 @@ df_list, param_list = util_tools.many_files(files)
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 ax.set_xscale('log'), ax.set_yscale('log')
 ax.spines['top'].set_visible(False), ax.spines['right'].set_visible(False)
-fontsize = 29
+fontsize = 39
 #ax1.set_xlim((0,0.16)), ax2.set_xlim((0,0.16))
 
 for data, params in zip(df_list, param_list):
@@ -33,14 +33,14 @@ for data, params in zip(df_list, param_list):
     ax.plot(kk, P0, 'x', markersize=17, color='teal')
     #ax2.plot(kk, kk*P2, '--', linewidth=0.8)
 
-ax.set_ylabel('$P(k)[ h^{-3}$Mpc$^3]$', fontsize=fontsize)#, ax2.set_title('$k*P_2(k)$')
-ax.set_xlabel('$k[h$Mpc$^{-1} ]$', fontsize=fontsize)#, ax2.set_title('$k*P_2(k)$')
+ax.set_ylabel('$\log_{10}P(k)[ h^{-3}$Mpc$^3]$', fontsize=fontsize)#, ax2.set_title('$k*P_2(k)$')
+ax.set_xlabel('$\log_{10}k[h$Mpc$^{-1} ]$', fontsize=fontsize)#, ax2.set_title('$k*P_2(k)$')
 ax.set_xticks((0.01, 0.05, 1e-1, 0.45))
 ax.set_xticklabels((0.01, 0.05, 1e-1, 0.45), fontsize=fontsize)
 yticks = (2600, 12500, 25000, 50000, 75000)
 ax.set_yticks(yticks)
-ax.set_yticklabels(yticks, fontsize=fontsize/1.3)
+ax.set_yticklabels(yticks, fontsize=fontsize)
 fig.set_tight_layout(True)
-plt.savefig('../figs/Pkrustico.png')
+plt.savefig('../figs/Pkrustico.pdf')
 
 
